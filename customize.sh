59fc0d3fba747aa6314a921fe7a246a4a75549ef
onokatio <onokatio@gmail.com>
2020-09-12 18:08:00 +0900

    support new magisk installation structure

--- customize.sh
+++ customize.sh
@@ -17,35 +17,16 @@
 ##########################################################################################
 
 ##########################################################################################
-# Configs
-##########################################################################################
-
-# Set to true if you need to enable Magic Mount
-# Most mods would like it to be enabled
-AUTOMOUNT=true
-
-# Set to true if you need to load system.prop
-PROPFILE=true
-
-# Set to true if you need post-fs-data script
-POSTFSDATA=false
-
-# Set to true if you need late_start service script
-LATESTARTSERVICE=false
-
-##########################################################################################
 # Installation Message
 ##########################################################################################
 
 # Set what you want to show when installing your mod
 
-print_modname() {
-   ui_print "*******************************"
-  ui_print "        Xperia XZ2 Sound System        "
-  ui_print "              by @sirdoha              "
-  ui_print "             V1.4 - Magisk             "
-  ui_print "*******************************"
-}
+ui_print "*******************************"
+ui_print "        Xperia XZ2 Sound System        "
+ui_print "              by @sirdoha              "
+ui_print "             V1.4 - Magisk             "
+ui_print "*******************************"
 
 ##########################################################################################
 # Replace list
@@ -53,15 +34,6 @@ print_modname() {
 
 # List all directories you want to directly replace in the system
 # Check the documentations for more info about how Magic Mount works, and why you need this
-
-# This is an example
-REPLACE="
-/system/app/Youtube
-/system/priv-app/SystemUI
-/system/priv-app/Settings
-/system/framework
-"
-
 # Construct your own list here, it will override the example above
 # !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
 REPLACE="
@@ -73,23 +45,7 @@ REPLACE="
 # Permissions
 ##########################################################################################
 
-set_permissions() {
-  # Only some special files require specific permissions
-  # The default permissions should be good enough for most cases
-
-  # Here are some examples for the set_perm functions:
-
-  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
-  # set_perm_recursive  $MODPATH/system/lib       0       0       0755            0644
-
-  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
-  # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
-  # set_perm  $MODPATH/system/bin/dex2oat         0       2000    0755         u:object_r:dex2oat_exec:s0
-  # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
-
-  # The following is default permissions, DO NOT remove
-  set_perm_recursive  $MODPATH  0  0  0755  0644
-}
+set_perm_recursive  $MODPATH  0  0  0755  0644
 
 ##########################################################################################
 # Custom Functions
@@ -101,3 +57,4 @@ set_permissions() {
 # difficult for you to migrate your modules to newer template versions.
 # Make update-binary as clean as possible, try to only do function calls in it.
 
+cp -af $TMPDIR/system.prop $MODPATH/system.prop
